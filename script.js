63dab5913d7d988b7606b0784030cc7a70a0efe8
veegas <mohamed.yasser.abdel.razek@gmail.com>
2016-05-21 04:43:21 +0200

    resolved file paths

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(window).load(function () {
   $(".loading-container").toggleClass("loading");
-  $(".main-container").toggleClass("loading");
+  $(".main-container").toggleClass("loading");  
 })
 
 
